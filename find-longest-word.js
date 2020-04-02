5e3f46b4e0b7ba5bc1b3c0d643b748806a25348a
Harry Chopra <harrychopra@gmail.com>
2020-04-02 14:11:21 +0100

    Refactored the solution

--- find-longest-word.js
+++ find-longest-word.js
@@ -1,11 +1,12 @@
 export const findLongestWordLength = (str) => {
 	let strArr = str.split(' ');
-	for (let i = 0; i < strArr.length - 1; i++) {
-		if (strArr[i].length > strArr[i + 1].length) {
-			[strArr[i], strArr[i + 1]] = [strArr[i + 1], strArr[i]];
+	let longest = 0;
+	for (let i = 0; i < strArr.length; i++) {
+		if (strArr[i].length > longest) {
+			longest = strArr[i].length;
 		}
 	}
-	return strArr[strArr.length - 1].length;
+	return longest;
 };
 
 findLongestWordLength('The quick brown fox jumped over the lazy dog');

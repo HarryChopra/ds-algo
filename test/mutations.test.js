218a93863fce9bad0384110813f3e40a516eebec
Harry Chopra <harrychopra@gmail.com>
2020-04-04 21:54:20 +0100

    Tests complete

---
+++ test/mutations.test.js
@@ -0,0 +1,40 @@
+import { mutation } from '../mutations';
+
+describe('Mutations', () => {
+	test('mutation(["hello", "hey"]) should return false.', () => {
+		expect(mutation(['hello', 'hey'])).toBe(false);
+	});
+	test('mutation(["hello", "Hello"]) should return true.', () => {
+		expect(mutation(['hello', 'Hello'])).toBe(true);
+	});
+	test('mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) should return true.', () => {
+		expect(mutation(['zyxwvutsrqponmlkjihgfedcba', 'qrstu'])).toBe(true);
+	});
+	test('mutation(["Mary", "Army"] should return true', () => {
+		expect(mutation(['Mary', 'Army'])).toBe(true);
+	});
+	test('mutation(["Mary", "Aarmy"]) should return true.', () => {
+		expect(mutation(['Mary', 'Aarmy'])).toBe(true);
+	});
+	test('mutation(["Alien", "line"]) should return true.', () => {
+		expect(mutation(['Alien', 'line'])).toBe(true);
+	});
+	test('mutation(["floor", "for"]) should return true.', () => {
+		expect(mutation(['floor', 'for'])).toBe(true);
+	});
+	test('mutation(["hello", "neo"]) should return false', () => {
+		expect(mutation(['hello', 'neo'])).toBe(false);
+	});
+	test('mutation(["voodoo", "no"]) should return false.', () => {
+		expect(mutation(['voodoo', 'no'])).toBe(false);
+	});
+	test('mutation(["ate", "date"] should return false.', () => {
+		expect(mutation(['ate', 'date'])).toBe(false);
+	});
+	test('mutation(["Tiger", "Zebra"]) should return false.', () => {
+		expect(mutation(['Tiger', 'Zebra'])).toBe(false);
+	});
+	test('mutation(["Noel", "Ole"]) should return true.', () => {
+		expect(mutation(['Noel', 'Ole'])).toBe(true);
+	});
+});
